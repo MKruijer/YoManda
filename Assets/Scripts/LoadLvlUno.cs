5e4884c7ac1993786dda9853b781dc25becf417c
MKruijer <45063316+MKruijer@users.noreply.github.com>
2019-02-07 11:17:19 +0100

    HomeScreen

---
+++ Assets/Scripts/LoadLvlUno.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLvlUno : MonoBehaviour {
+	public string LevelUno;
+
+	public void LoadSceneUno() {
+		SceneManager.LoadScene(LevelUno);
+	}
+}
