f1777a5d4697ffef789d18033ebe296afcb63ac1
MKruijer <45063316+MKruijer@users.noreply.github.com>
2019-03-07 11:10:51 +0100

    updatoe Cameroe

--- Assets/Scripts/CamreaController.cs
+++ Assets/Scripts/CamreaController.cs
@@ -23,13 +23,31 @@ public class CamreaController : MonoBehaviour {
 
 		pivot.transform.position = target.transform.position;
 		pivot.transform.parent = target.transform;
+
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
 		
-		transform.position = target.position - offset;
+		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+		target.Rotate(0, horizontal, 0);
 
+		float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+		pivot.Rotate(-vertical, 0, 0);
+
+
+		float desiredYAngle = target.eulerAngles.y;
+		float desiredXAngle = pivot.eulerAngles.x;
+		Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+		transform.position = target.position - (rotation * offset);
+
+		transform.LookAt(target);
+
+		if(transform.position.y < target.position.y)
+		{
+			transform.position = new Vector3(transform.position.x, target.position.y -.5f, transform.position.z);
+		}
 		transform.LookAt(target);
 	}
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,17 +21,22 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		moveDirection = new Vector3(Input.GetAxis("Horizontal") * MovementSpeed, moveDirection.y, Input.GetAxis("Vertical") * MovementSpeed);
+
+		
+		//moveDirection = new Vector3(Input.GetAxis("Horizontal") * MovementSpeed, moveDirection.y, Input.GetAxis("Vertical") * MovementSpeed);
+		float yStore = moveDirection.y;
+		moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+		moveDirection = moveDirection.normalized * MovementSpeed;
+		moveDirection.y = yStore;
 
 		if(controller.isGrounded)	//zorgt ervoor dat je niet oneindig kan springen maar 1x totdat je weer de grond aanraakt.
 		{
+			moveDirection.y = 0f;
 			if(Input.GetButtonDown("Jump"))
 		{
 			moveDirection.y = jumpForce;
 		}
 		}
-
-		
 
 		moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
 		controller.Move(moveDirection * Time.deltaTime);
